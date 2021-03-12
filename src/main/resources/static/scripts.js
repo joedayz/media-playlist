bebf28eb1b206d2143f0349e9653163b8bf02bd4
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-03-11 23:23:06 -0500

    implementar jquery y agregar modal para agregar playlist

---
+++ src/main/resources/static/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#playlist > tbody").append(
+        '<tr style="background-color: teal" class="teal"><td colspan="3"><span data-toggle="modal" data-target="#add-playlist" style="color: white" title="Create Playlist" class="fa fa-plus fa-3x" aria-hidden="true"> </span></td></tr>'
+    );
+});
