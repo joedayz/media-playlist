178ac18423c20f7ece57c92f613ee2ba4b93e51a
Jose Amadeo Diaz Diaz <jadiaz@farmaciasperuanas.pe>
2021-03-11 22:00:03 -0500

    primera version

---
+++ src/main/java/io/educative/mediaapp/MediaappApplication.java
@@ -0,0 +1,13 @@
+package io.educative.mediaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/educative/mediaapp/MediaappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.educative.mediaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
